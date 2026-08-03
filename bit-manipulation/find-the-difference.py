062b930e436fa4f8b57f6d94d80853aecab9390c
Arpit Singh <arpitsingh8534@gmail.com>
2026-08-03 21:19:15 +0530

    LC-find-the-difference [Runtime: N/A; Memory: N/A]

--- bit-manipulation/find-the-difference.py
+++ bit-manipulation/find-the-difference.py
@@ -10,6 +10,4 @@ class Solution:
             for ch in t:
                 if ch not in count or count[ch] == 0:
                     return ch
-                
-            count[ch] -= 1
-                
+            count[ch] -= 1  
