c4f985ff58f3e320b26d264a99f60c4bd6f2a284
Arpit Singh <arpitsingh8534@gmail.com>
2026-08-03 21:15:14 +0530

    LC-find-the-difference [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/find-the-difference.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+            count = {}
+            for ch in s:
+                if ch in count:
+                    count[ch] = count[ch]+1
+                else:
+                    count[ch] = 1
+            
+            for ch in t:
+                if ch not in count:
+                    return ch
+                
