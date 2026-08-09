8c30e8620b5f2bcaad8c811b373003d929f8f243
Arpit Singh <arpitsingh8534@gmail.com>
2026-08-09 12:20:37 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(char.lower() for char in s if char.isalpha())
+        s = "".join(char.lower() for char in s if char.isalnum())
         t = s[::-1]
         if s == t:
             return True
