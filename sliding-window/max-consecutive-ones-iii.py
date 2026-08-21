d7b81ba8c5cc39828205ed769ed42513070d87e4
Arpit Singh <arpitsingh8534@gmail.com>
2026-08-21 14:20:15 +0530

    LC-max-consecutive-ones-iii [Runtime: N/A; Memory: N/A]

--- sliding-window/max-consecutive-ones-iii.py
+++ sliding-window/max-consecutive-ones-iii.py
@@ -6,14 +6,16 @@ class Solution:
 
         for r in range(len(nums)):
             if nums[r] == 1:
+                mlen = max(r-l+1,mlen)
                 continue
             elif nums[r] == 0 and count >0:
                 count-=1
-                mlen = max(r-l,mlen)
+                mlen = max(r-l+1,mlen)
             elif nums[r] == 0 and count==0:
-                mlen = max(r-l,mlen)
+                
                 while nums[l] != 0:
                     l+=1
                 l+=1
+                mlen = max(r-l+1,mlen)
                 
         return mlen
