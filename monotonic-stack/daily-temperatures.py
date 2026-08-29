86c961a13840ae64bb0f41d3a162bf49762314c4
Arpit Singh <arpitsingh8534@gmail.com>
2026-08-29 20:35:43 +0530

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

--- monotonic-stack/daily-temperatures.py
+++ monotonic-stack/daily-temperatures.py
@@ -1,25 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-
-        result = [0] * len(temperatures)
+        res = [0] * len(temperatures)
         stack = []
-
-        for i in range(len(temperatures)):
-
-            current_temperature = temperatures[i]
-
-            while len(stack) > 0:
-
-                previous_temperature = stack[-1][0]
-                previous_index = stack[-1][1]
-
-                if current_temperature <= previous_temperature:
-                    break
-
-                stack.pop()
-
-                result[previous_index] = i - previous_index
-
-            stack.append([current_temperature, i])
-
-        return result
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i-stackInd)
+            stack.append([t,i])
+        return res
